7919432bea7e6a296d5bae13c784efe23fc2d055
SwagaRuney <84145639+SwagaRuney@users.noreply.github.com>
2025-09-30 13:41:50 -0400

    Add files via upload

--- monster fight.py
+++ monster fight.py
@@ -1,17 +1,25 @@
 import random
 import time
 turn = 1
-health = 100
+health = 1000
 curDamage = random.randint(5, 20) 
 smallmodifier = 0
 
+
+exclaimtable = { "Doesn't even hurt...", "Keep trying! You'll get nowhere!", "pff...", "ow!", "just give up!"}
+othersayingstuff = ['says: ', 'exclaims: ', 'quotes: ', 'scoffs: ']
+print('PRESS ENTER TO BEGIN THE FIGHT')
+input()
+print('the battle begins! (press enter to attack)')
 while health > 0:
     health -= curDamage * smallmodifier
+    input()
     if health <= 0:
-        print("you win, you also dealt", int(health), "more damage than you should of. not a bad thing, good job!")
+        print("The boss screams: AHHHHHH! Curse you . . !\nyou win, you also dealt", int(health), "more damage than you should of. \nThat's not a bad thing, good job!")
         break
     print("Turn: ", turn, "\nDamage Dealt! \namount:  ", int(curDamage), "\nHealth left: ", int(health))
-    time.sleep(1)
+    print('\nDETAILS : \nThe boss', othersayingstuff[random.randint(0, 3)], random.choice(list(exclaimtable)), '\nYou take another shot . . .\n')
+    
     curDamage = random.randint(5, 20) 
     smallmodifier += 0.4
     turn += 1
